0abbcb2762bc3ed14c4c5c6f86640705fb3638a5
taewankung <taewankung@gmail.com>
2017-01-21 01:14:28 +0700

    add register_controller

---
+++ run_naga/widgets/register_controller.py
@@ -0,0 +1,32 @@
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import Screen
+from multiprocessing import Process
+
+from os.path import expanduser
+import os
+import threading
+import subprocess
+
+Builder.load_file(os.path.dirname(__file__)+'/register_controller.kv')
+
+class RegisterController(Screen):
+    def __init__(self,name='register'):
+        super().__init__(name=name)
+
+#    def on_login(self):
+    def on_register_user(self,user,password,email,first_name,last_name):
+        response = self.manager.client_game.user.register(user,password,email,first_name,last_name)
+        args = response['args']
+        self.manager.current = 'login'
+    def on_back(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login'
+        #self.manager.transition.direction = 'left'
+#        logined = response['responses']['loggedin']
+#        if logined:
+#            self.manager.client_id = response['client_id']
+#            self.manager.user = args['username']
+#            self.manager.token = response['responses']['token']
+#            self.manager.current = 'lobby'
